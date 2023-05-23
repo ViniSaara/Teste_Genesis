35ccab8ed28420a79491fc47276ac53e14e987ea
Vinícius Sabino <viniaraujobh2000@gmail.com>
2023-05-23 12:00:44 -0300

    Criando arquivo readme.

---
+++ TesteGenesis/Readme.mk
@@ -0,0 +1,17 @@
+/*
+	Desenvolvido por Vinícius Araújo
+*/
+
+1- O projeto TesteGenesis.Api é o backend
+2- O projeto TesteGenesis.Api.Test é o projeto de teste do backend
+3- Na pasta TesteGenesis.Front (no diretório do GitHub) é o front. Obs.: Não consegui adicionar a pasta do front angular aqui no VS. mas ela está no github.
+4- Ao executar a webapi o endereço pra mim aqui ficou o seguinte:
+	- https://localhost:44328
+
+5- Caso queira executar o swagger segue a rota abaixo. Obs.: Verifique a porta usada
+	https://localhost:44328/swagger/ui/index
+
+6- Ao executar o projeto Angular o endereço foi:
+	http://localhost:4200/
+
+7- Não consegui fazer o front comunicar com a webapi. Acho que foi algum problema de CORS que não deu tempo de resolver.
