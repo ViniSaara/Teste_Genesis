cb2af4b997a2e3024c2c69b86eb4ab9cf32dada0
Vinícius Sabino <viniaraujobh2000@gmail.com>
2023-05-24 09:43:19 -0300

    .

--- TesteGenesis/Readme.mk
+++ TesteGenesis/Readme.mk
@@ -4,7 +4,7 @@
 
 1- O projeto TesteGenesis.WebApi é o backend
 2- O projeto TesteGenesis.Api.Test é o projeto de teste do backend
-3- Na pasta TesteGenesis.Front (no diretório do GitHub) é o front. Obs.: Não consegui adicionar a pasta do front angular aqui no VS. mas ela está no github.
+3- Na pasta TesteGenesis.Front (no diretório do GitHub) é o front.
 4- Ao executar a webapi o endereço pra mim aqui ficou o seguinte:
 	- https://localhost:44399
 
@@ -18,3 +18,9 @@
 	- Print_0_Meses.png
 	- Print_5_meses_valor_inical_1000.png
 	- Print_valor_inicial_0.png
+
+8- O valor inicial deve ser informado com um '.' (ponto) para separar as casas decimais. Exemplo: 1564.31
+
+
+
+
